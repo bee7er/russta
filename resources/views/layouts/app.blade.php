0d76de5c4801e311f889cb17fa2ceded76791689
Brian Etheridge <contact_bee@yahoo.com>
2016-07-16 15:17:30 +0000

    Sort out the responsive design somewhat.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 <div class="wrapper">
     @include('partials.nav')
 
-    <div class="container">
+    <div class="container-fluid">
         @yield('content')
 
     </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,21 +7,14 @@
     @if(count($resources)>0)
         <div class="row">
             @for ($i = 0; $i < count($resources); $i++)
-                <div class="col-sm-3">
-                    <div class="row" style="height:153px;text-align:left;border:0px solid red;">
-                        <!-- NB Using the resource type to spcify the base part of the url -->
-                        <a href="{{url($resources[$i]->type .'/'.$resources[$i]->id.'')}}">
-                                <img class="col-sm-12" src="{!! url('/appfiles/resource/'.$resources[$i]->thumb) !!}"
-                                     style="margin:0;padding:0;width:100%;height:100%;border:0px solid red;"
-                                    title="{!! $resources[$i]->name !!}">
-                        </a>
-                    </div>
+                <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-1">
+                    <!-- NB Using the resource type to spcify the base part of the url -->
+                    <a href="{{url($resources[$i]->type .'/'.$resources[$i]->id.'')}}">
+                            <img class="" src="{!! url('/appfiles/resource/'.$resources[$i]->thumb) !!}"
+                                 style="margin:0;padding:0;width:100%;height:153px;border:0px solid red;"
+                                title="{!! $resources[$i]->name !!}">
+                    </a>
                 </div>
-
-                @if((($i + 1) % 4) <= 0)
-                    <div class="clearfix"></div>
-                @endif
-
             @endfor
         </div>
     @endif
