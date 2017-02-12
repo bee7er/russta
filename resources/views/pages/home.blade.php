72c6da47fbd9461bb1859e011edbcfd84a0d86d2
bee7er <contact_bee@yahoo.com>
2017-02-12 14:29:08 +0000

    Add top links

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,6 +17,7 @@
                 @endfor
             </div>
         </div>
+        <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
     @endif
 
     <div id="about" class="panel-title">about</div>
@@ -27,56 +28,51 @@
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 about-right">
             <p>Hello!</p>
             <p>I’m a Brighton based director, designer and animator with a wide range of animation skills, including 2D and 3D character animation motion graphics.</p>
-            <p>Most recently I have moved to the sunny and creative mixing pot that is Brighton and I am now in the process of setting up my own independent animatin studio!</p>
+            <p>Most recently I have moved to the sunny and creative mixing pot that is Brighton and I am now in the process of setting up my own independent animation studio!</p>
             <p>If you'd like to get in touch about a project I can help with or just a friendly chat, I'd love to hear from you!</p>
-            <p class="center-text">contact@ruselletheridge.com</p>
-            <p class="center-text">+44(0)7790 330 102</p>
+            <p class="center-text">email.address</p>
+            <p class="center-text">telephone.number</p>
         </div>
     </div>
-    <div class="clearfix"></div>
-
+    <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
+    
     <div id="contact" class="panel-title">contact</div>
     <div class="row contact-row-container">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <p>Have a project in mind, or just want to say hi?</p>
             <p>I’d love to hear from you!.</p>
-            <p class="center-wrap-text">russ@squaresquare.com</p>
-            <p class="center-text">+44(0)7790 330 102</p>
+            <p class="center-wrap-text">email.address</p>
+            <p class="center-text">telephone.number</p>
             <p class="center-text">Follow me!</p>
             <p class="center-text">
-                <a target="_blank" href="#">
-                    <i class="social-icon fa fa-youtube" title="Share on YouTube"></i>
-                </a>
-                <a target="_blank" href="#">
-                    <i class="social-icon fa fa-facebook" title="Share on Facebook"></i>
-                </a>
-                <a target="_blank" href="#">
-                    <i class="social-icon fa fa-twitter" title="Share on Twitter"></i>
-                </a>
-                <a target="_blank" href="#">
-                    <i class="social-icon fa fa-google-plus" title="Share on Google+"></i>
-                </a>
+                <a target="_blank" href="https://dribbble.com/russ_ether"><img src="img/social/dribble.png" class="social-icon" title="Share on dribble" /></a>
+                <a target="_blank" href="https://www.facebook.com/profile.php?id=100013591591149"><img src="img/social/facebook.png" class="social-icon" title="Share on facebook" /></a>
+                <a target="_blank" href="https://www.instagram.com/russ_ether/"><img src="img/social/instagram.png" class="social-icon" title="Share on instagram" /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/russether"><img src="img/social/linkedin.png" class="social-icon" title="Share on linkedin" /></a>
+                <a target="_blank" href="http://russether.tumblr.com/"><img src="img/social/tumblr.png" class="social-icon" title="Share on tumblr" /></a>
+                <a target="_blank" href="https://twitter.com/russ_ether"><img src="img/social/twitter.png" class="social-icon" title="Share on twitter" /></a>
+                <a target="_blank" href="https://vimeo.com/russether"><img src="img/social/vimeo.png" class="social-icon" title="Share on vimeo" /></a>
             </p>
         </div>
     </div>
-    <div class="clearfix"></div>
+    <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
 
     <div id="merch" class="panel-title">merch</div>
     <div class="row merch-row-container">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <p>Check out my range of merchandising.</p>
             <p>Interesting and fun at affordable prices.</p>
-            <p class="center-wrap-text">russ@squaresquare.com</p>
-            <p class="center-text">+44(0)7790 330 102</p>
+            <p class="center-wrap-text">email.address</p>
+            <p class="center-text">telephone.number</p>
             <p class="center-text">Buy me!</p>
         </div>
     </div>
-    <div class="clearfix"></div>
+    <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
 
     <h2 id="fish-tank" class="panel-title">fish tank</h2>
     <div class="row fish-tank-row-container">
             <img src="{{config('app.base_url')}}img/fishtank_placeholder.png" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" />
     </div>
-    <div class="clearfix"></div>
+    <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,35 +1,37 @@
 
+<div id="top">&nbsp;</div>
+
 <div class="row" style="width:80%; border:0px solid red;margin:20px auto 0 auto;">
     <div class="hidden-xs hidden-sm col-md-12 col-lg-12 header-block">
-        <a href="#home" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                 onmouseout="$(this).removeClass('white-link-hover')">work</span></a>
+        <span onclick="gotoPage('home');" onmouseover="$(this).addClass('white-link-hover');"
+                                                 onmouseout="$(this).removeClass('white-link-hover')">work</span>
         <img src="img/square.png" />
-        <a href="#about" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                  onmouseout="$(this).removeClass('white-link-hover')">about</span></a>
+        <span onclick="gotoPage('about');" onmouseover="$(this).addClass('white-link-hover')"
+                                                  onmouseout="$(this).removeClass('white-link-hover')">about</span>
         <a href="#home" class="white-link"><img src="{{config('app.base_url')}}img/logo.png" /></a>
-        <a href="#contact" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
+        <span onclick="gotoPage('contact');" onmouseover="$(this).addClass('white-link-hover')"
                                                     onmouseout="$(this).removeClass('white-link-hover')
-                                                    ">contact</span></a>
+                                                    ">contact</span>
         <img src="img/square.png" />
-        <a href="#merch" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                  onmouseout="$(this).removeClass('white-link-hover')">merch</span></a>
+        <span onclick="gotoPage('merch');" onmouseover="$(this).addClass('white-link-hover')"
+                                                  onmouseout="$(this).removeClass('white-link-hover')">merch</span>
     </div>
     <div class="hidden-xs col-sm-12 hidden-md hidden-lg header-block">
         <div class="row" style="margin-bottom:20px;">
             <a href="#home" class="white-link"><img src="{{config('app.base_url')}}img/logo.png" /></a>
         </div>
-        <a href="#home" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                 onmouseout="$(this).removeClass('white-link-hover')">work</span></a>
+        <span onclick="gotoPage('home');" onmouseover="$(this).addClass('white-link-hover');"
+              onmouseout="$(this).removeClass('white-link-hover')">work</span>
         <img src="img/square.png" />
-        <a href="#about" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                  onmouseout="$(this).removeClass('white-link-hover')">about</span></a>
+        <span onclick="gotoPage('about');" onmouseover="$(this).addClass('white-link-hover')"
+              onmouseout="$(this).removeClass('white-link-hover')">about</span>
         <img src="img/square.png" />
-        <a href="#contact" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                    onmouseout="$(this).removeClass('white-link-hover')
-                                                    ">contact</span></a>
+        <span onclick="gotoPage('contact');" onmouseover="$(this).addClass('white-link-hover')"
+              onmouseout="$(this).removeClass('white-link-hover')
+                                                    ">contact</span>
         <img src="img/square.png" />
-        <a href="#merch" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                  onmouseout="$(this).removeClass('white-link-hover')">merch</span></a>
+        <span onclick="gotoPage('merch');" onmouseover="$(this).addClass('white-link-hover')"
+              onmouseout="$(this).removeClass('white-link-hover')">merch</span>
     </div>
     <div class="col-xs-12 hidden-sm hidden-md hidden-lg header-block">
         <div class="row" style="margin-bottom:20px;">
@@ -38,28 +40,39 @@
         <table style="margin-bottom:10px;margin:0 auto 0 auto;">
             <tbody>
             <tr>
-                <td style="text-align:right;"><a href="#home" class="white-link"><span onmouseover="$(this).addClass
-                ('white-link-hover')"
-                                                             onmouseout="$(this).removeClass('white-link-hover')">work</span></a>
+                <td style="text-align:right;">
+                    <span onclick="gotoPage('home');" onmouseover="$(this).addClass('white-link-hover');"
+                          onmouseout="$(this).removeClass('white-link-hover')">work</span>
                 </td>
                 <td style="text-align:center;"><img src="img/square.png" /></td>
-                <td style="text-align:left;"><a href="#about" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                                                       onmouseout="$(this).removeClass('white-link-hover')
-                                                      ">about</span></a>
+                <td style="text-align:left;">
+                    <span onclick="gotoPage('about');" onmouseover="$(this).addClass('white-link-hover')"
+                          onmouseout="$(this).removeClass('white-link-hover')">about</span>
                 </td>
             </tr>
             <tr>
-                <td style="text-align:right;"><a href="#contact" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                                                          onmouseout="$(this).removeClass('white-link-hover')
-                                                        ">contact</span></a>
+                <td style="text-align:right;">
+                    <span onclick="gotoPage('contact');" onmouseover="$(this).addClass('white-link-hover')"
+                          onmouseout="$(this).removeClass('white-link-hover')
+                                                    ">contact</span>
                 </td>
                 <td style="text-align:center;"><img src="img/square.png" /></td>
-                <td style="text-align:left;"><a href="#merch" class="white-link"><span onmouseover="$(this).addClass('white-link-hover')"
-                                                                                       onmouseout="$(this).removeClass('white-link-hover')
-                                                      ">merch</span></a>
+                <td style="text-align:left;">
+                    <span onclick="gotoPage('merch');" onmouseover="$(this).addClass('white-link-hover')"
+                          onmouseout="$(this).removeClass('white-link-hover')">merch</span>
                 </td>
             </tr>
             </tbody>
         </table>
     </div>
 </div>
+
+<script type="text/javascript">
+    function gotoPage(aid) {
+        scrollToAnchor(aid);
+    }
+    function scrollToAnchor(aid) {
+        var aTag = $("div[id='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    }
+</script>
