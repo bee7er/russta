03030980c6bbcf00ddc20a2157e437bc0c3a7594
bee7er <contact_bee@yahoo.com>
2017-02-15 11:36:35 +0000

    Stab two at the experimental template

--- resources/views/pages/template.blade.php
+++ resources/views/pages/template.blade.php
@@ -3,6 +3,8 @@
 
 @section('content')
 
+    <body style="background-color: magenta;" />
+
     <div class="template-title">bathroom boarder</div>
     <div id="video-panel" class="row template-row-container">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -10,6 +12,29 @@
             auto;width: 100%;height:350px;" src="https://player.vimeo.com/video/137499366" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </div>
     </div>
-    <div class="go-top clearfix" onclick="scrollToAnchor('top');">#top</div>
+
+    <div class="row template-row-container template-sub-container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 template-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum nulla ac magna semper scelerisque. Pellentesque fermentum elit et felis laoreet, non vulputate purus laoreet. Mauris blandit felis rhoncus neque tempor, a lobortis justo dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed at orci quam. Sed sit amet metus scelerisque, maximus massa ac, commodo massa. Praesent ac rutrum sapien. Proin rutrum sodales placerat. Pellentesque ligula urna, gravida vel suscipit vitae, volutpat ac nibh. Ut gravida vehicula felis et maximus. Aliquam condimentum consectetur turpis, ac fringilla felis dapibus vitae.
+        </div>
+    </div>
+
+    <div class="row template-row-container template-sub-container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 template-text">
+            <img src="img/pegs.png" width="100%" />
+        </div>
+    </div>
+
+    <div class="template-credits-title">credits</div>
+    <div class="row template-credits-row-container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="template-credits-label">Director</div>
+            <div class="template-credits-text">Russ Etheridge</div>
+            <div class="template-credits-label">Produced</div>
+            <div class="template-credits-text">Animade</div>
+            <div class="template-credits-label">Sound</div>
+            <div class="template-credits-text">Mutant Jukebox</div>
+        </div>
+    </div>
 
 @endsection
