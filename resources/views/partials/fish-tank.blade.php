cb1d9d78f3168ecb2b2d1a4f51ee12d2d6970560
bee7er <contact_bee@yahoo.com>
2017-02-22 12:49:34 +0000

    Add 404 page

--- resources/views/partials/fish-tank.blade.php
+++
@@ -1,7 +0,0 @@
-
-<div class="row" style="border:0px solid red;">
-    <div style="border:0 solid black;padding:0;margin:20px auto 0 auto;background-color:#fff;width:80%;">
-        <img src="{{config('app.base_url')}}img/fishtank_placeholder.png"  class="col-xs-12 col-sm-12 col-md-12
-        col-lg-12" />
-    </div>
-</div>
